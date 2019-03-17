795123d362110e8225a6e676374e577db1aee6c6
aspanio <aspanio@outlook.com>
2019-03-17 12:29:58 +0200

    1

---
+++ build/precache-manifest.933a8258cad936e33672f9beb60fa3ba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "731ad59376b0dcb3c93dac3de5ec281d",
+    "url": "/static/media/login.731ad593.svg"
+  },
+  {
+    "revision": "ab283a1d69326d76fe61",
+    "url": "/static/css/main.f5b58ddd.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/Proxima Nova Regular.410504d4.otf"
+  },
+  {
+    "revision": "7526f95bb9a4401895b2",
+    "url": "/static/js/2.c6a4d952.chunk.js"
+  },
+  {
+    "revision": "dd6c52e2674343dee0f79f8d3818d7d8",
+    "url": "/static/media/kid.dd6c52e2.png"
+  },
+  {
+    "revision": "1d58b29e846c186074f844a37609463a",
+    "url": "/static/media/logo.1d58b29e.svg"
+  },
+  {
+    "revision": "ab283a1d69326d76fe61",
+    "url": "/static/js/main.9c249498.chunk.js"
+  },
+  {
+    "revision": "41158ffaed80747831b97ad16164a3cd",
+    "url": "/static/media/karpachoff.41158ffa.png"
+  },
+  {
+    "revision": "65da7f6802e7322a730c6b7e8fdce6a3",
+    "url": "/static/media/baby.65da7f68.svg"
+  },
+  {
+    "revision": "8a9b0b7ce2ba8d8fc3bd8cb965cc0da1",
+    "url": "/static/media/arrow.8a9b0b7c.svg"
+  },
+  {
+    "revision": "a25906616517ec348c130a06178817c9",
+    "url": "/static/media/icon.a2590661.svg"
+  },
+  {
+    "revision": "b2f90b602a9f5c2da56ad4ff3074c9ca",
+    "url": "/static/media/8289.b2f90b60.otf"
+  },
+  {
+    "revision": "7526f95bb9a4401895b2",
+    "url": "/static/css/2.7b2cedcc.chunk.css"
+  },
+  {
+    "revision": "6a0af5a8769151f120cfef4ef6dfe2e9",
+    "url": "/index.html"
+  }
+];
